e0c0771e245a328eeecbbf915a7f2f62a94a68af
RochinelPanus <rochinel@panus.cm>
2025-12-16 16:07:44 +0100

    authentication/registration
    Resolve merge conflict on the MarketPrice.Domain and
    MarketPrice.Services projects

---
+++ MarketPrice.Domain/Authentication/Commands/RegistrationCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPrice.Domain.Authentication.Commands
+{
+    public class RegistrationCommand
+    {
+        public required string FirstName { get; set; }
+        public required string FamilyName { get; set; }
+        public string?  OtherName { get; set; }
+        public required string  AccountType { get; set; }
+        public required string EmailAddress { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get; set; }
+
+    }
+
+}
