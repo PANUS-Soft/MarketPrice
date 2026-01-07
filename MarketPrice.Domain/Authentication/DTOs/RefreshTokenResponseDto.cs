94f865eff10c804b99ba138cb2514c022e394f66
Tertullien19 <tertullien@panus.cm>
2026-01-07 10:21:44 +0100

    Added the command and response dto necessary for refreshing the user's access token

---
+++ MarketPrice.Domain/Authentication/DTOs/RefreshTokenResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPrice.Domain.Authentication.DTOs
+{
+    public class RefreshTokenResponseDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
