75bdea4feeb41cfea8210b9115013bf499fc0ca2
Atankeu Tchakoute Ange <natanahel@panus.cm>
2026-01-15 13:41:05 +0100

    Created CreatePosition, Location commands and PositionResponseDto

---
+++ MarketPrice.Domain/Position/Commands/LocationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPrice.Domain.Position.Commands
+{
+    public class LocationCommand
+    {
+        public int RegionId { get; set; }
+        public required string Town { get; set; }
+        public required string Quarter { get; set; }
+        public string? Street { get; set; }
+    }
+}
