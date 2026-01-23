e2ef539175e29665202ee31426e4f1a2e6bae491
RochinelPanus <rochinel@panus.cm>
2026-01-23 13:36:44 +0100

    update the dto and add the commodityTypeId in the maketRespons response for the market insight

--- MarketPrice.Domain/Market/Dtos/MarketInsightResponseDto.cs
+++ MarketPrice.Domain/Market/Dtos/MarketInsightResponseDto.cs
@@ -6,15 +6,19 @@ using System.Threading.Tasks;
 
 namespace MarketPrice.Domain.Market.Dtos
 {
-    public class MarketDepthResponseDto
+    public class MarketInsightResponseDto
     {
 
         public Guid CommodityId { get; set; }
+        public Guid CommodityTypeId { get; set; }
         public string CommodityName { get; set; }
         public string CommodityCode { get; set; }
         public string ImageUrl { get; set; }
         public decimal BestBid { get; set; }
         public decimal BestOffer { get; set; }
+
+        public bool BidUp { get; set; }
+        public bool OfferDown { get; set; }
 
     }
     
