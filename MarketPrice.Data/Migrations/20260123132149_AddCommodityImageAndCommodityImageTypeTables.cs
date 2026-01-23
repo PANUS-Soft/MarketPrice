e8da8b80b5737f61ac281fb9f8b27d348348c08c
Atankeu Tchakoute Ange <natanahel@panus.cm>
2026-01-23 16:24:26 +0100

    Seed CommodityTypeImage and CommodityImage tables

--- MarketPrice.Data/Migrations/20260123132149_AddCommodityImageAndCommodityImageTypeTables.cs
+++ MarketPrice.Data/Migrations/20260123132149_AddCommodityImageAndCommodityImageTypeTables.cs
@@ -15,7 +15,7 @@ namespace MarketPrice.Data.Migrations
                 name: "CommodityTypeImageId",
                 table: "CommodityTypes",
                 type: "uniqueidentifier",
-                nullable: true,
+                nullable: false,
                 defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
             migrationBuilder.AddColumn<DateTimeOffset>(
@@ -43,7 +43,7 @@ namespace MarketPrice.Data.Migrations
                 name: "CommodityImageId",
                 table: "Commodities",
                 type: "uniqueidentifier",
-                nullable: true,
+                nullable: false,
                 defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
 
             migrationBuilder.AddColumn<DateTimeOffset>(
---
+++ MarketPrice.Data/Migrations/20260123143709_SeedCommodityImage.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPrice.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCommodityImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                                migrationBuilder.Sql(@"
+                        -- BLACK_BEANS
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'black_beans.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\black_beans.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Black Beans';
+
+                        -- CRACKED_EGUSI
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'cracked_egusi.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\cracked_egusi.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Cracked Egusi';
+
+                        -- DRY_CORN
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'dry_corn.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\dry_corn.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Dry Corn';
+
+                        -- FRESH_CORN
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'fresh_corn.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\fresh_corn.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Fresh Corn';
+
+                        -- GINGER
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'ginger.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\ginger.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Ginger';
+
+                        -- PALM_OIL
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'palm_oil.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\palm_oil.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Palm Oil';
+
+                        -- RED_BEANS
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'red_beans.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\red_beans.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Red Beans';
+
+                        -- RED_ONION
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'red_onion.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\red_onion.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Red Onions';
+
+                        -- UNCRACKED_EGUSI
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'uncracked_egusi.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\uncracked_egusi.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'Uncracked Egusi';
+
+                        -- WHITE_BEANS
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'white_beans.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\white_beans.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'White Beans';
+
+                        -- WHITE_ONION
+                        INSERT INTO CommodityImage (CommodityImageId,  ImageData, ContentType, FileName)
+                        SELECT NEWID(),  Image.BulkColumn, 'image/png', 'white_onion.png'
+                        FROM Commodities c
+                        CROSS APPLY OPENROWSET(BULK 'C:\MarketPrice\MarketPrice.Data\Images\Commodities\white_onion.png', SINGLE_BLOB) AS Image
+                        WHERE CommodityName = 'White Onions';
+                        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CommodityImage");
+        }
+    }
+    
+}
+
