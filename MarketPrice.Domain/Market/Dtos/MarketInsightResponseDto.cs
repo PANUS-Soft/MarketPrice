167e4b327b219fe0533ad2f56868cdb8f8efe085
RochinelPanus <rochinel@panus.cm>
2026-01-23 12:43:30 +0100

    update the dto response for the market insight

---
+++ MarketPrice.Domain/Market/Dtos/MarketInsightResponseDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPrice.Domain.Market.Dtos
+{
+    public class MarketDepthResponseDto
+    {
+
+        public Guid CommodityId { get; set; }
+        public string CommodityName { get; set; }
+        public string CommodityCode { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal BestBid { get; set; }
+        public decimal BestOffer { get; set; }
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
