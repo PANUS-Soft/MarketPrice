b966887b0a9e318dcc8dd33345a05e37b7fbb576
NELSONPanus123 <nelson@panus.cm>
2026-01-29 12:30:21 +0100

    fix: updated the lot size for all the commodities and the lookup data value and textenglish

--- MarketPrice.Data/Migrations/20260115112157_SeedingCommodity.cs
+++ MarketPrice.Data/Migrations/20260115112157_SeedingCommodity.cs
@@ -24,34 +24,34 @@ namespace MarketPrice.Data.Migrations
         DECLARE @LitreId UNIQUEIDENTIFIER = (SELECT TOP 1 UnitOfMeasureId FROM UnitOfMeasures WHERE UnitOfMeasureCodeEnglish = 'L');
 
         -- 3 Insert specific Commodities
-        -- Grains / Corn
+        -- Grains / Corn (Updated to 50kg)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @CornType, @KgId, 'Dry Corn', 100, 365, 'High starch content'),
+        VALUES (NEWID(), @CornType, @KgId, 'Dry Corn', 50, 365, 'High starch content'),
                (NEWID(), @CornType, @KgId, 'Fresh Corn', 50, 7, 'Perishable; keep cool');
 
-        -- Legumes / Beans
+        -- Legumes / Beans (Updated to 50kg)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @BeanType, @KgId, 'Red Beans', 100, 730, 'Very Good when fried'),
-               (NEWID(), @BeanType, @KgId, 'White Beans', 100, 730, 'Commonly eaten with tubers or rice'),
-               (NEWID(), @BeanType, @KgId, 'Black Beans', 100, 730, 'Good in pounded meals like pounded Irish potatoes');
+        VALUES (NEWID(), @BeanType, @KgId, 'Red Beans', 50, 730, 'Very Good when fried'),
+               (NEWID(), @BeanType, @KgId, 'White Beans', 50, 730, 'Commonly eaten with tubers or rice'),
+               (NEWID(), @BeanType, @KgId, 'Black Beans', 50, 730, 'Good in pounded meals like pounded Irish potatoes');
 
-        -- Vegetables / Onions
+        -- Vegetables / Onions (Updated to 50kg)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @OnionType, @KgId, 'White Onions', 40, 30, 'Mild flavor'),
-               (NEWID(), @OnionType, @KgId, 'Red Onions', 40, 60, 'Sharp flavor; stores longer');
+        VALUES (NEWID(), @OnionType, @KgId, 'White Onions', 50, 30, 'Mild flavor'),
+               (NEWID(), @OnionType, @KgId, 'Red Onions', 50, 60, 'Sharp flavor; stores longer');
 
-        -- Roots / Ginger
+        -- Roots / Ginger (Updated to 25kg)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @GingerType, @KgId, 'Dry Ginger', 30, 365, 'Dehydrated roots');
+        VALUES (NEWID(), @GingerType, @KgId, 'Dry Ginger', 25, 365, 'Dehydrated roots');
 
-        -- Oilseeds / Egusi
+        -- Oilseeds / Egusi (Updated to 25kg)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @EgusiType, @KgId, 'Uncracked Egusi', 50, 365, 'Protected by shell'),
-               (NEWID(), @EgusiType, @KgId, 'Cracked Egusi', 20, 90, 'Shelled; needs dry storage');
+        VALUES (NEWID(), @EgusiType, @KgId, 'Uncracked Egusi', 25, 365, 'Protected by shell'),
+               (NEWID(), @EgusiType, @KgId, 'Cracked Egusi', 25, 90, 'Shelled; needs dry storage');
 
-        -- Oils / Palm Oil
+        -- Oils / Palm Oil (Updated to 20L)
         INSERT INTO [Commodities] (CommodityId, CommodityTypeId, UnitOfMeasureId, CommodityName, LotSize, ShelfLifeInDays, Notes)
-        VALUES (NEWID(), @OilType, @LitreId, 'Palm Oil', 25, 545, 'Pure extract');
+        VALUES (NEWID(), @OilType, @LitreId, 'Palm Oil', 20, 545, 'Pure extract');
     ");
         }
 
