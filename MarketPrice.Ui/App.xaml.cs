ba52a7024d1a787e7d0095d00eb9bf997682d8b7
NELSONPanus123 <nelson@panus.cm>
2026-01-30 11:52:22 +0100

    Feature: done implementing the profile menu view

--- MarketPrice.Ui/App.xaml.cs
+++ MarketPrice.Ui/App.xaml.cs
@@ -33,23 +33,25 @@ namespace MarketPrice.Ui
 
         private async Task HandleStartupNavigationAsync()
         {
-            var hasOnboarded = Preferences.Get("HasCompletedOnboarding", false);
-            if (!hasOnboarded)
-            {
-                await Shell.Current.GoToAsync("//Onboarding");
-                return;
-            }
+            await Shell.Current.GoToAsync("//Profile");
+            return;
+            //var hasOnboarded = Preferences.Get("HasCompletedOnboarding", false);
+            //if (!hasOnboarded)
+            //{
+            //    await Shell.Current.GoToAsync("//Onboarding");
+            //    return;
+            //}
 
-            bool hasValidSession = await _sessionService.ValidateAndRefreshSessionAsync();
+            //bool hasValidSession = await _sessionService.ValidateAndRefreshSessionAsync();
 
-            if (hasValidSession)
-            {
-                await Shell.Current.GoToAsync("//Home");
-            }
-            else
-            {
-                await Shell.Current.GoToAsync("//Welcome");
-            }
+            //if (hasValidSession)
+            //{
+            //    await Shell.Current.GoToAsync("//Home");
+            //}
+            //else
+            //{
+            //    await Shell.Current.GoToAsync("//Welcome");
+            //}
         }
 
         //protected override Window CreateWindow(IActivationState? activationState)
