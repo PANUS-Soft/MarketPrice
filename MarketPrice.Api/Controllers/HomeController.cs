a8b87f40fd112ad807983a6039db520ba56b41d7
Atankeu Tchakoute Ange <natanahel@panus.cm>
2026-02-02 13:35:16 +0100

    Uncomment Authorize attribute in Home, Markets and Positions controllers

--- MarketPrice.Api/Controllers/HomeController.cs
+++ MarketPrice.Api/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MarketPrice.Api.Controllers
         /// Loads home market data for all commodity types
         /// </summary>
 
-        //[Authorize]
+        [Authorize]
         [HttpGet(ApiRoutes.LOAD_HOME_DATA)]
         public async Task<ActionResult<List<LoadHomeResponseDto>>> LoadHome()
         {
