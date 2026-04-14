be6a17f043d6ce193681ff0711640aa4bc02a438
Ka-rol7 <Karol@panus.cm>
2026-04-14 16:02:20 +0100

    Implement update home view mocks.

---
+++ MarketPrice.Ui/Converters/TrendArrowConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPrice.Ui.Converters
+{
+    public class TrendArrowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool up && up ? "▲" : "▼";
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class TrendColorConverter : IValueConverter
+    {
+        public bool IsOffer { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool trendUp = value is bool b && b;
+            if (IsOffer)
+                return trendUp ? Color.FromArgb("#22C55E") : Color.FromArgb("#EF4444");
+            else
+                return trendUp ? Color.FromArgb("#22C55E") : Color.FromArgb("#EF4444");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+           => throw new NotImplementedException();
+    }
+}
